a8a81c93188f3934642fc0d2b8ced5c27bd95daf
Patrik Höggren <phoggren@gmail.com>
2016-09-21 23:39:59 +0200

    Commandline arguments in making...

--- bt-util/main.swift
+++ bt-util/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print(IOBluetoothHostController.controllers())
+//var arguments = CommandLine.arguments
 
+var arguments = ["/usur/sjsjs/ssjsj", "status"]
+arguments.removeFirst()
+
+if arguments.count == 1 {
+    // no args!
+}
+
+var arg = arguments.removeFirst()
+
+switch arg {
+    case "status", "--status", "-s":
+        let prefs = IOBluetoothPreferences()
+        print(String(format: "Status: %@", prefs.poweredOn ? "On" : "Off"))
+    
+    case "version", "--version", "-v":
+    break
+    
+    case "help", "--help", "-h":
+    break
+    
+    default:
+    break
+}
